e40acb56f2dc00b913cecd813affa887fbb96b38
Alfaraby Indra <alfarabyindra@gmail.com>
2024-03-18 22:58:41 +0700

    upload codewars wpu 2024

---
+++ 18-Personalized Message.js
@@ -0,0 +1,11 @@
+//level latihan codewars 8kyu
+//versi simple
+function greet(name, owner) {
+  // Add code here
+  if (name === owner) {
+    return "Hello boss";
+  }
+  if (name !== owner) {
+    return "Hello guest";
+  }
+}
---
+++ 19-Sum mixed Array.js
@@ -0,0 +1,9 @@
+//level latihan codewars 8kyu
+//versi simple
+function sumMix(x) {
+  let total = 0;
+  for (let i = 0; i < x.length; i++) {
+    total += parseInt(x[i]);
+  }
+  return total;
+}
